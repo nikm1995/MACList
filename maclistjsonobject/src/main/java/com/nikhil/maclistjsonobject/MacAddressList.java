e7e17d00fa2342f3513db6c4c800de79048eec29
NIKHIL <morenikhil5@gmail.com>
2019-02-14 21:33:53 +0530

    active

--- maclistjsonobject/src/main/java/com/nikhil/maclistjsonobject/MacAddressList.java
+++ maclistjsonobject/src/main/java/com/nikhil/maclistjsonobject/MacAddressList.java
@@ -24,7 +24,7 @@ public class MacAddressList {
 
     public JsonObject getList() {
         try {
-            
+
             InputStream Is=context.getAssets().open(javaFile);
             JsonReader jsonReader= Json.createReader(Is);
             list= jsonReader.readObject();
