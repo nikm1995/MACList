62c0de9f20766264c501e7c4751958ffcce6a1cc
NIKHIL <morenikhil5@gmail.com>
2019-02-14 17:08:09 +0530

    active

--- maclistjsonobject/src/main/java/ListJson.java
+++ maclistjsonobject/src/main/java/ListJson.java
@@ -1,4 +1,23 @@
+import android.content.Context;
+
+import org.json.JSONObject;
+
+import javax.json.JsonObject;
+
 public class ListJson {
 
-MACJSONLIST m;
+private MACJSONLIST m;
+    private Context context;
+    public ListJson(MACJSONLIST m) {
+        this.m = m;
+    }
+    public ListJson(Context context){
+        this.context=context;
+    }
+
+    public JSONObject getList(){
+        MACJSONLIST macjsonlist=new MACJSONLIST(this.context);
+       return macjsonlist.getList();
+    }
+
 }
